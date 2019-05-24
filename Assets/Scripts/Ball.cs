b83b2503a692ea2ced2526689b3cc5faf09aaa41
tungunity96 <especsily@gmail.com>
2019-05-24 14:52:58 +0700

    init project

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Ball : MonoBehaviour {
+  public void Start()
+  {
+    var myPath = GetComponent<DOTweenPath>();
+    var myPathTween = GetComponent<DOTweenPath>().tween;
+    myPathTween.timeScale = 0f;
+  }
+
+
+}
